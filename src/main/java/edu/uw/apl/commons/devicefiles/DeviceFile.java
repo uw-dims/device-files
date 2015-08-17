7fac457ddd7b86bdf4a6e9532f6edb8c61167275
Stuart Maclean <stuart@apl.uw.edu>
2015-08-17 15:35:58 -0700

    Improve comments, add new unit tests

--- src/main/java/edu/uw/apl/commons/devicefiles/DeviceFile.java
+++ src/main/java/edu/uw/apl/commons/devicefiles/DeviceFile.java
@@ -1,9 +1,6 @@
 package edu.uw.apl.commons.devicefiles;
 
-import java.io.BufferedInputStream;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
 import java.io.IOException;
 
 import edu.uw.apl.nativelibloader.NativeLoader;
@@ -11,11 +8,13 @@ import edu.uw.apl.nativelibloader.NativeLoader;
 /**
  * @author Stuart Maclean
  *
- * Capture device files, e.g. /dev/sda, in a Java class.  We use
- * native methods to derive DeviceFile ids (vendor info, serial number
- * etc) and disk sizes (since File.length() returns 0 on a device
- * file).  There is no 'regular' Java API for obtaining a disk's
- * vendor/serial details, which is very platform-specific.
+ * Capture device files, e.g. /dev/sda, in a Java class.  Device file
+ * here nearly always means 'whole disks', like /dev/sda or 'C:'.
+
+ * We use native methods to derive DeviceFile ids (vendor info, serial
+ * number etc) and disk sizes (since File.length() returns 0 on a
+ * device file).  There is no 'regular' Java API for obtaining a
+ * disk's vendor/serial details, which is very platform-specific.
  *
  * We use NativeLibLoader to manage loading any .so/.dylib/.dll
  */
@@ -26,10 +25,10 @@ public class DeviceFile {
 			throw new IllegalArgumentException( "Null file!" );
 
 		/*
-		  Fail early with IOException if no such file, before jni...
+		  Fail early with IOException if f unreadable...
 		*/
-		FileInputStream fis = new FileInputStream( f );
-		fis.close();
+		if( !f.canRead() )
+			throw new IOException( "Unreadable: " + f.getPath() );
 
 		// Java reports 0 for the size of a device file...
 		long len = f.length();
@@ -39,14 +38,38 @@ public class DeviceFile {
 		disk = f;
 	}
 
+	/**
+	 * @return the File as passed to us in the constructor
+	 */
 	public File getSource() {
 		return disk;
 	}
-	
+
+	/**
+	 * @return The size of the device file, in bytes. Uses native code
+	 * to derive the answer.  The following, Java-only code, will
+	 * <b>not</b> work (it will give 0):
+	 *
+	 * long sz = new File( "/dev/sda" ).length();
+	 */
 	public long size() {
 		return size( disk.getPath() );
 	}
 
+	/**
+	 * @return a string, concatenated from various platform-specific strings
+	 * like
+	 * 
+	 * vendor, e.g. Seagate, Western Digital etc
+	 *
+	 * productID, e.g. a model number as named by the vendor
+	 *
+	 * serial number, hopefully unique across all disks
+	 *
+	 * The actual concatenation process is arbitrary, currently we are
+	 * using '-' chars as a delimiter, and replacing any whitespace
+	 * chars found with '_'.
+	 */
 	public String getID() {
 		String v = vendorID( disk.getPath() );
 		if( v != null )
--- src/test/java/edu/uw/apl/commons/devicefiles/DeviceFileTest.java
+++ src/test/java/edu/uw/apl/commons/devicefiles/DeviceFileTest.java
@@ -1,20 +1,40 @@
 package edu.uw.apl.commons.devicefiles;
 
 import java.io.File;
+import java.io.IOException;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class DeviceFileTest {
 
 
 	@Test
-	public void test1() throws Exception {
+	public void testSDA() throws Exception {
 		File f = new File( "/dev/sda" );
 		if( !f.canRead() )
 			return;
 		DeviceFile df = new DeviceFile( f );
 		System.out.println( "Size: " + df.size() );
 		System.out.println( "Size: " + df.getID() );
+	}
+	
+	@Test
+	public void testSDAUnreadable() throws Exception {
+		File f = new File( "/dev/sda" );
+		if( f.canRead() )
+			return;
+		try {
+			DeviceFile df = new DeviceFile( f );
+			fail( "Expected IOException building " + f );
+		} catch( IOException ioe ) {
+		}
+	}
+	
+	@Test
+	public void testJavaSizeIsZero() throws Exception {
+		File f = new File( "/dev/sda" );
+		assertFalse( f.length() > 0 );
 	}
 }
 
